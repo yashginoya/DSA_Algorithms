437df82014647b303d2623e9e8449053e5bd747d
Yash Ginoya <yashginoya2003@gmail.com>
2023-07-09 03:25:41 +0530

    Create bellmanFord.cpp

---
+++ Graph_Algo/bellmanFord.cpp
@@ -0,0 +1,36 @@
+// single source shortest path algo
+// TC = O(N*M)     or     O(V*E) 
+
+void bellmanFord(int source,int n,vector<int> &dist,vector<int> &parent,vector<vector<pair<int,int>>> &adj,set<int> &negCycle){
+    // fill(all(dist),INF);
+    dist.assign(n,INF);
+    parent.assign(n,-1);
+    dist[source] = 0;
+
+    vector<pair<int,pair<int,int>>> edges;
+    for(int i = 0 ; i<n;i++){
+        for(auto j : adj[i]){
+            edges.push_back({j.second,{i,j.first}});
+        }
+    }
+
+    for(int i = 0; i < n-1 ; i++){
+        for(auto j : edges){
+            if(dist[j.second.second] > dist[j.second.first] + j.first){
+                dist[j.second.second] = dist[j.second.first] + j.first;
+                parent[j.second.second] = j.second.first;
+            }
+        }
+    }
+
+
+    // Checking for negative cycle
+    for(int i = 0;i<n;i++){
+        for(auto j:edges){
+            if(dist[j.second.second] > dist[j.second.first] + j.first){
+                dist[j.second.second] = dist[j.second.first] + j.first;
+                negCycle.insert(j.second.second);
+            }
+        }
+    }
+}
