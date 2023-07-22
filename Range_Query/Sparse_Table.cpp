af1f9f2c0d612a0998403ef6b5135769222b1286
Yash Ginoya <yashginoya2003@gmail.com>
2023-07-22 18:41:11 +0530

    Update Sparse_Table.cpp

--- Range_Query/Sparse_Table.cpp
+++ Range_Query/Sparse_Table.cpp
@@ -14,7 +14,10 @@ struct SparseTable {
         return min(x, y);
     }
 
-    SparseTable(const vector<int> &a) : n(size(a)), LOG(__lg(n)), table(n, vector<int>(LOG + 1, -1)) {
+    SparseTable(const vector<int> &a) {
+        n = a.size();
+        LOG = log2(n);
+        table.resize(n,vector<int>(LOG+1,-1);
         for (int j = 0; j <= LOG; j++) {
             for (int i = 0; i <= n - (1 << j); i++) {
                 if (j == 0) table[i][j] = a[i];
@@ -24,7 +27,8 @@ struct SparseTable {
     }
 
     int query(int l, int r) {
-        int j = __lg(r - l + 1);
+        // int j = __lg(r - l + 1);
+        int j = log2(r-l+1);
         return merge(table[l][j], table[r - (1 << j) + 1][j]);
     }
 };
